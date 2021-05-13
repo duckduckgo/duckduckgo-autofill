d0fb913a198acf1fdb91245b364f62ca085472bf
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-05-13 16:23:21 +0200

    Remove unused grunt stuff
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,8 @@
 module.exports = function (grunt) {
     'use strict'
 
-    // grunt.loadNpmTasks('grunt-exec')
+    grunt.loadNpmTasks('grunt-exec')
     grunt.loadNpmTasks('grunt-eslint')
-    // grunt.loadNpmTasks('grunt-githooks')
     grunt.loadNpmTasks('grunt-browserify')
     grunt.loadNpmTasks('grunt-contrib-watch')
 
@@ -28,15 +27,7 @@ module.exports = function (grunt) {
             },
             target: 'src/**/*.js'
         },
-        exec: {
-            excludeBuild: 'git update-index --assume-unchanged dist/autofill.js'
-        },
-        githooks: {
-            all: {
-                // Will create `./git/hooks/pre-commit`. It will build and commit the output file.
-                'pre-push': 'npm run build && git add dist/autofill.js && git commit -m "Add build file"'
-            }
-        },
+        exec: {},
         /**
          * Run predefined tasks whenever watched files are added,
          * modified or deleted.
