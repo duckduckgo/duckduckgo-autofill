61a51faac13e45a5ce5dd5a9d5d9e6be64d93792
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-06-11 14:12:14 +0200

    Abstract a generic Tooltip class
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/UI/DDGAutofill.js
+++ src/UI/DDGAutofill.js
@@ -1,25 +1,25 @@
 const {
     isApp,
     formatAddress,
-    getDaxBoundingBox,
     safeExecute,
     escapeXML
 } = require('../autofill-utils')
+const Tooltip = require('./Tooltip')
 
-class DDGAutofill {
+class EmailAutofill extends Tooltip {
     constructor (input, associatedForm, Interface) {
-        const shadow = document.createElement('ddg-autofill').attachShadow({mode: 'closed'})
-        this.host = shadow.host
+        super()
+        this.shadow = document.createElement('ddg-autofill').attachShadow({mode: 'closed'})
+        this.host = this.shadow.host
         this.input = input
         this.associatedForm = associatedForm
         this.addresses = Interface.getLocalAddresses()
-        this.animationFrame = null
 
         const includeStyles = isApp
             ? `<style>${require('./styles/DDGAutofill-styles.js')}</style>`
             : `<link rel="stylesheet" href="${chrome.runtime.getURL('public/css/autofill.css')}" crossorigin="anonymous">`
 
-        shadow.innerHTML = `
+        this.shadow.innerHTML = `
 ${includeStyles}
 <div class="wrapper">
     <div class="tooltip" hidden>
@@ -35,15 +35,11 @@ ${includeStyles}
         </button>
     </div>
 </div>`
-        this.wrapper = shadow.querySelector('.wrapper')
-        this.tooltip = shadow.querySelector('.tooltip')
-        this.usePersonalButton = shadow.querySelector('.js-use-personal')
-        this.usePrivateButton = shadow.querySelector('.js-use-private')
-        this.addressEl = shadow.querySelector('.js-address')
-        this.stylesheet = shadow.querySelector('link, style')
-        // Un-hide once the style is loaded, to avoid flashing unstyled content
-        this.stylesheet.addEventListener('load', () =>
-            this.tooltip.removeAttribute('hidden'))
+        this.wrapper = this.shadow.querySelector('.wrapper')
+        this.tooltip = this.shadow.querySelector('.tooltip')
+        this.usePersonalButton = this.shadow.querySelector('.js-use-personal')
+        this.usePrivateButton = this.shadow.querySelector('.js-use-private')
+        this.addressEl = this.shadow.querySelector('.js-address')
 
         this.updateAddresses = (addresses) => {
             if (addresses) {
@@ -51,98 +47,6 @@ ${includeStyles}
                 this.addressEl.textContent = formatAddress(addresses.personalAddress)
             }
         }
-
-        // Get the alias from the extension
-        Interface.getAddresses().then(this.updateAddresses)
-
-        this.top = 0
-        this.left = 0
-        this.transformRuleIndex = null
-        this.updatePosition = ({left, top}) => {
-            // If the stylesheet is not loaded wait for load (Chrome bug)
-            if (!shadow.styleSheets.length) return this.stylesheet.addEventListener('load', this.checkPosition)
-
-            this.left = left
-            this.top = top
-
-            if (this.transformRuleIndex && shadow.styleSheets[this.transformRuleIndex]) {
-                // If we have already set the rule, remove it…
-                shadow.styleSheets[0].deleteRule(this.transformRuleIndex)
-            } else {
-                // …otherwise, set the index as the very last rule
-                this.transformRuleIndex = shadow.styleSheets[0].rules.length
-            }
-
-            const newRule = `.wrapper {transform: translate(${left}px, ${top}px);}`
-            shadow.styleSheets[0].insertRule(newRule, this.transformRuleIndex)
-        }
-
-        this.append = () => document.body.appendChild(shadow.host)
-        this.append()
-        this.lift = () => {
-            this.left = null
-            this.top = null
-            document.body.removeChild(this.host)
-        }
-
-        this.remove = () => {
-            window.removeEventListener('scroll', this.checkPosition, {passive: true, capture: true})
-            this.resObs.disconnect()
-            this.mutObs.disconnect()
-            this.lift()
-        }
-
-        this.checkPosition = () => {
-            if (this.animationFrame) {
-                window.cancelAnimationFrame(this.animationFrame)
-            }
-
-            this.animationFrame = window.requestAnimationFrame(() => {
-                const {left, bottom} = getDaxBoundingBox(this.input)
-
-                if (left !== this.left || bottom !== this.top) {
-                    this.updatePosition({left, top: bottom})
-                }
-
-                this.animationFrame = null
-            })
-        }
-        this.resObs = new ResizeObserver(entries => entries.forEach(this.checkPosition))
-        this.resObs.observe(document.body)
-        this.count = 0
-        this.ensureIsLastInDOM = () => {
-            // If DDG el is not the last in the doc, move it there
-            if (document.body.lastElementChild !== this.host) {
-                this.lift()
-
-                // Try up to 5 times to avoid infinite loop in case someone is doing the same
-                if (this.count < 15) {
-                    this.append()
-                    this.checkPosition()
-                    this.count++
-                } else {
-                    // Reset count so we can resume normal flow
-                    this.count = 0
-                    console.info(`DDG autofill bailing out`)
-                }
-            }
-        }
-        this.mutObs = new MutationObserver((mutationList) => {
-            for (const mutationRecord of mutationList) {
-                if (mutationRecord.type === 'childList') {
-                    // Only check added nodes
-                    mutationRecord.addedNodes.forEach(el => {
-                        if (el.nodeName === 'DDG-AUTOFILL') return
-
-                        this.ensureIsLastInDOM()
-                    })
-                }
-            }
-            this.checkPosition()
-        })
-        this.mutObs.observe(document.body, {childList: true, subtree: true, attributes: true})
-        window.addEventListener('scroll', this.checkPosition, {passive: true, capture: true})
-
         this.usePersonalButton.addEventListener('click', (e) => {
             if (!e.isTrusted) return
             e.stopImmediatePropagation()
@@ -160,7 +64,16 @@ ${includeStyles}
                 Interface.refreshAlias()
             })
         })
+
+        // Get the alias from the extension
+        Interface.getAddresses().then(this.updateAddresses)
+
+        this.init()
+        this.append()
+        this.resObs.observe(document.body)
+        this.mutObs.observe(document.body, {childList: true, subtree: true, attributes: true})
+        window.addEventListener('scroll', this.checkPosition, {passive: true, capture: true})
     }
 }
 
-module.exports = DDGAutofill
+module.exports = EmailAutofill
---
+++ src/UI/Tooltip.js
@@ -0,0 +1,110 @@
+const {getDaxBoundingBox} = require('../autofill-utils')
+
+const updatePosition = function ({left, top}) {
+    const shadow = this.shadow
+    // If the stylesheet is not loaded wait for load (Chrome bug)
+    if (!shadow.styleSheets.length) return this.stylesheet.addEventListener('load', this.checkPosition)
+
+    this.left = left
+    this.top = top
+
+    if (this.transformRuleIndex && shadow.styleSheets[this.transformRuleIndex]) {
+        // If we have already set the rule, remove it…
+        shadow.styleSheets[0].deleteRule(this.transformRuleIndex)
+    } else {
+        // …otherwise, set the index as the very last rule
+        this.transformRuleIndex = shadow.styleSheets[0].rules.length
+    }
+
+    const newRule = `.wrapper {transform: translate(${left}px, ${top}px);}`
+    shadow.styleSheets[0].insertRule(newRule, this.transformRuleIndex)
+}
+
+const checkPosition = function () {
+    if (this.animationFrame) {
+        window.cancelAnimationFrame(this.animationFrame)
+    }
+
+    this.animationFrame = window.requestAnimationFrame(() => {
+        const {left, bottom} = getDaxBoundingBox(this.input)
+
+        if (left !== this.left || bottom !== this.top) {
+            this.updatePosition({left, top: bottom})
+        }
+
+        this.animationFrame = null
+    })
+}
+
+const ensureIsLastInDOM = function () {
+    // If DDG el is not the last in the doc, move it there
+    if (document.body.lastElementChild !== this.host) {
+        this.lift()
+
+        // Try up to 5 times to avoid infinite loop in case someone is doing the same
+        if (this.count < 15) {
+            this.append()
+            this.checkPosition()
+            this.count++
+        } else {
+            // Reset count so we can resume normal flow
+            this.count = 0
+            console.info(`DDG autofill bailing out`)
+        }
+    }
+}
+
+class Tooltip {
+    append () {
+        document.body.appendChild(this.host)
+    }
+    remove () {
+        window.removeEventListener('scroll', this.checkPosition, {passive: true, capture: true})
+        this.resObs.disconnect()
+        this.mutObs.disconnect()
+        this.lift()
+    }
+    lift () {
+        this.left = null
+        this.top = null
+        document.body.removeChild(this.host)
+    }
+    checkPosition = checkPosition.bind(this)
+    updatePosition = updatePosition.bind(this)
+    ensureIsLastInDOM = ensureIsLastInDOM.bind(this)
+    mutObs = new MutationObserver((mutationList) => {
+        for (const mutationRecord of mutationList) {
+            if (mutationRecord.type === 'childList') {
+                // Only check added nodes
+                mutationRecord.addedNodes.forEach(el => {
+                    if (el.nodeName === 'DDG-AUTOFILL') return
+
+                    this.ensureIsLastInDOM()
+                })
+            }
+        }
+        this.checkPosition()
+    })
+    init () {
+        this.animationFrame = null
+        this.top = 0
+        this.left = 0
+        this.transformRuleIndex = null
+
+        this.resObs = new ResizeObserver(entries => entries.forEach(this.checkPosition))
+        this.count = 0
+
+        this.stylesheet = this.shadow.querySelector('link, style')
+        // Un-hide once the style is loaded, to avoid flashing unstyled content
+        this.stylesheet.addEventListener('load', () =>
+            this.tooltip.removeAttribute('hidden'))
+    }
+    setupStylesheet () {
+        this.stylesheet = this.shadow.querySelector('link, style')
+        // Un-hide once the style is loaded, to avoid flashing unstyled content
+        this.stylesheet.addEventListener('load', () =>
+            this.tooltip.removeAttribute('hidden'))
+    }
+}
+
+module.exports = Tooltip
