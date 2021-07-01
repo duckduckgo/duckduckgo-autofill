d9122239ea7f966c5c5c4f41893f210b4a2e04a3
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-07-01 13:22:36 +0200

    Remove existing login detection
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/autofill.js
+++ src/autofill.js
@@ -9,10 +9,6 @@
 
     // chrome is only present in desktop browsers
     if (typeof chrome === 'undefined') {
-        const {isApp} = require('./autofill-utils')
-        if (isApp) {
-            require('./Form/existing-login-detection.js')
-        }
         inject()
     } else {
         // Check if the site is marked to skip autofill
