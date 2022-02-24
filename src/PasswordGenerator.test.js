0aa2050d857b71ff3838b4cc1394325b9aa1b252
Shane Osbourne <shane.osbourne8@gmail.com>
2022-02-24 14:05:57 +0000

    test for PasswordGenerator

--- src/PasswordGenerator.test.js
+++ src/PasswordGenerator.test.js
@@ -1,17 +1,17 @@
 const {PasswordGenerator} = require('./PasswordGenerator')
 
-describe("PasswordGenerator", () => {
+describe('PasswordGenerator', () => {
     it('generates a password once', () => {
-        const pwg = new PasswordGenerator();
-        expect(pwg.generated).toBe(false);
+        const pwg = new PasswordGenerator()
+        expect(pwg.generated).toBe(false)
         const pws = [
             pwg.generate(),
             pwg.generate(),
             pwg.generate(),
             pwg.generate(),
-            pwg.generate(),
-        ];
-        expect(new Set(pws).size).toBe(1);
-        expect(pwg.generated).toBe(true);
+            pwg.generate()
+        ]
+        expect(new Set(pws).size).toBe(1)
+        expect(pwg.generated).toBe(true)
     })
 })
