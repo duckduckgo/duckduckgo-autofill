b716548c8b28042961f071e0e03c3c1c1ce177e4
Shane Osbourne <shane.osbourne8@gmail.com>
2022-02-26 10:06:33 +0000

    no http

--- packages/password/scripts/compare-rules.js
+++ packages/password/scripts/compare-rules.js
@@ -1,14 +1,14 @@
-const prev = require("../rules.json");
+const prev = require('../rules.json')
 
 module.exports = async ({github, context}) => {
-    const res = [];
-    const remote = await fetchRemote();
+    const res = []
+    const remote = await fetchRemote()
     for (let [domain, value] of Object.entries(prev)) {
         if (!remote[domain]) {
             res.push(`local, not in remote: ${domain}`)
         } else {
             if (remote[domain]['password-rules'] !== value['password-rules']) {
-                res.push(`DIFF ${domain}: `);
+                res.push(`DIFF ${domain}: `)
                 res.push(`\tlocal: ${value['password-rules']}`)
                 res.push(`\tremote: ${remote[domain]['password-rules']}`)
             }
@@ -16,14 +16,13 @@ module.exports = async ({github, context}) => {
     }
     for (let [domain, value] of Object.entries(remote)) {
         if (!prev[domain]) {
-
             res.push(`in remote, not in local: ${domain}`)
-            res.push(`\trules: ${value['password-rules']}`);
+            res.push(`\trules: ${value['password-rules']}`)
         }
     }
-    return res.join('\n');
+    return res.join('\n')
 }
 
-function fetchRemote(github) {
-    return github.request('https://raw.githubusercontent.com/apple/password-manager-resources/main/quirks/password-rules.json');
+function fetchRemote (github) {
+    return github.request('https://raw.githubusercontent.com/apple/password-manager-resources/main/quirks/password-rules.json')
 }
