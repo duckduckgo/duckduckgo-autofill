76aaba5521ecfa77dc530b06c0ee57334723166b
Shane Osbourne <shane.osbourne8@gmail.com>
2022-07-13 11:22:12 +0100

    Added support for passing an optional Abort Signal

--- packages/device-api/lib/device-api.js
+++ packages/device-api/lib/device-api.js
@@ -4,9 +4,10 @@
 export class DeviceApiTransport {
     /**
      * @param {import("./device-api-call.js").DeviceApiCall} _deviceApiCall
+     * @param {CallOptions} [_options]
      * @returns {Promise<any>}
      */
-    async send (_deviceApiCall) {
+    async send (_deviceApiCall, _options) {
         return undefined
     }
 }
@@ -15,6 +16,9 @@ export class DeviceApiTransport {
  * This is the base Sender class that platforms can will implement.
  *
  * Note: The 'handle' method must be implemented, unless you also implement 'send'
+ *
+ * @typedef CallOptions
+ * @property {AbortSignal} [signal]
  */
 export class DeviceApi {
     /** @type {DeviceApiTransport} */
@@ -26,21 +30,23 @@ export class DeviceApi {
     /**
      * @template {import("./device-api-call").DeviceApiCall} D
      * @param {D} deviceApiCall
+     * @param {CallOptions} [options]
      * @returns {Promise<ReturnType<D['validateResult']>['success']>}
      */
-    async request (deviceApiCall) {
+    async request (deviceApiCall, options) {
         deviceApiCall.validateParams()
-        let result = await this.transport.send(deviceApiCall)
+        let result = await this.transport.send(deviceApiCall, options)
         let processed = deviceApiCall.preResultValidation(result)
         return deviceApiCall.validateResult(processed)
     }
     /**
      * @template {import("./device-api-call").DeviceApiCall} P
      * @param {P} deviceApiCall
+     * @param {CallOptions} [options]
      * @returns {Promise<void>}
      */
-    async notify (deviceApiCall) {
+    async notify (deviceApiCall, options) {
         deviceApiCall.validateParams()
-        await this.transport.send(deviceApiCall)
+        return this.transport.send(deviceApiCall, options)
     }
 }
