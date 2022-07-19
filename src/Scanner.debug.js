febb55a41a2b1aacf7772cd6c3e9ceefa9dbcbf7
Shane Osbourne <shane.osbourne8@gmail.com>
2022-07-19 07:19:34 +0100

    Added script + example HTML for scanner debugging (#188)
    * Added script + example HTML for scanner debugging
    
    * docs for scanner debugging
    
    * Added submit buttons
    
    * lint

---
+++ src/Scanner.debug.js
@@ -0,0 +1,64 @@
+import { createScanner } from './Scanner.js'
+
+/**
+ * Scanner.debug.js
+ *
+ * Load this script into any HTML to debug Scanning logic
+ */
+
+/**
+ * Mock just enough of the device interface to prevent errors.
+ */
+const mockInterface = {
+    settings: {
+        availableInputTypes: {
+            credentials: true,
+            identities: true
+        },
+        featureToggles: {
+            inputType_credentials: true,
+            inputType_identities: true,
+            inputType_creditCards: true
+        }
+    },
+    globalConfig: {
+        isDDGApp: true
+    },
+    getLocalIdentities () {
+        return []
+    },
+    isDeviceSignedIn () {
+        return false
+    },
+    attachTooltip (...args) {
+        console.log('device.attachTooltip', args)
+    }
+}
+
+// @ts-ignore
+const scanner = createScanner(mockInterface, {
+    initialDelay: 0
+})
+
+scanner.init()
+
+setTimeout(() => {
+    console.group('forms')
+    for (const [formElement, formInstance] of scanner.forms) {
+        const { isLogin, isSignup, formAnalyzer, inputs, submitButtons } = formInstance
+        console.log(formElement)
+        console.log({
+            isLogin,
+            isSignup,
+            submitButtons,
+            autofillSignal: formAnalyzer.autofillSignal,
+            signals: formAnalyzer.signals
+        })
+        console.group('inputs')
+        for (let input of inputs.all) {
+            console.log(input)
+        }
+        console.groupEnd()
+    }
+    console.groupEnd()
+}, 500)
