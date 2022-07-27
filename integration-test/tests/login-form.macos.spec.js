548a270e5e1f307600ee023c18ed2c0239616e67
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2022-07-27 12:50:45 +0200

    Skip unreliable test
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- integration-test/tests/login-form.macos.spec.js
+++ integration-test/tests/login-form.macos.spec.js
@@ -102,7 +102,7 @@ test.describe('Auto-fill a login form on macOS', () => {
         server.close()
     })
     test.describe('without getAvailableInputTypes API', () => {
-        test('with in-page HTMLTooltip', async ({page}) => {
+        test.skip('with in-page HTMLTooltip', async ({page}) => {
             await testLoginPage(page, server)
         })
         test('with overlay', async ({page}) => {
