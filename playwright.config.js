d9fb214fd59bdcbb9045de45a0a9d58fb2c07eb4
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2022-07-27 12:32:26 +0200

    Retries tweak (again)
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ const config = {
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 6 : 0,
+    retries: process.env.CI ? 5 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : 6,
     // workers: 1,
