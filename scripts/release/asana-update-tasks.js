17d49d259392aa7e4d1e05ebf51e145d54d18d4d
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2022-10-26 16:44:08 +0200

    Add asana-update-tasks.js
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

---
+++ scripts/release/asana-update-tasks.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+const Asana = require('asana')
+const {replaceAllInString} = require('./release-utils.js')
+const {getLink} = require('./release-utils.js')
+const {wrapInLi} = require('./release-utils.js')
+
+const ASANA_ACCESS_TOKEN = process.env.ASANA_ACCESS_TOKEN
+const prUrls = {
+    android: process.env.ANDROID_PR_URL || 'error',
+    bsk: process.env.BSK_PR_URL || 'error',
+    ios: process.env.IOS_PR_URL || 'error',
+    macos: process.env.MACOS_PR_URL || 'error'
+}
+const asanaOutputRaw = process.env.ASANA_OUTPUT || '{}'
+const asanaOutput = JSON.parse(asanaOutputRaw)
+
+let asana
+
+const setupAsana = () => {
+    asana = Asana.Client.create({
+        'defaultHeaders': {
+            'Asana-Enable': 'new_project_templates,new_user_task_lists'
+        }
+    }).useAccessToken(ASANA_ACCESS_TOKEN)
+}
+
+const run = async () => {
+    setupAsana()
+
+    const platformEntries = Object.entries(asanaOutput)
+    for (const [platformName, platformObj] of platformEntries) {
+        if (!platformObj.taskGid || !prUrls[platformName]) continue
+
+        const { html_notes: notes } = await asana.tasks.getTask(platformObj.taskGid, { opt_fields: 'html_notes' })
+
+        /** @type {[[RegExp, string]]} */
+        const changes = [
+            [/\[\[pr_url]]/, getLink(prUrls[platformName], platformObj.displayName + ' PR')]
+        ]
+
+        if (platformName === 'bsk') {
+            // also substitute the ios and macos placeholders
+            const markup =
+                `${wrapInLi(getLink(prUrls.ios, 'iOS PR:'))}${wrapInLi(getLink(prUrls.macos, 'macOS PR:'))}`
+            changes.push(
+                [/\[\[extra_content]]/, markup]
+            )
+        } else {
+            changes.push(
+                [/<li>\[\[extra_content]]<\/li>/, '']
+            )
+        }
+
+        const updatedNotes = replaceAllInString(notes, changes)
+
+        await asana.tasks.updateTask(platformObj.taskGid, { html_notes: updatedNotes })
+    }
+}
+
+run().catch((e) => {
+    // The Asana API returns errors in e.value.errors. If that's undefined log whatever else we got
+    console.error(e.value?.errors || e)
+    process.exit(1)
+})
