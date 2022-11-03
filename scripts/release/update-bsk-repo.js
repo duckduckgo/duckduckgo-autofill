c5969584b13a5215fb952f6b00f214b7a1385cd1
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2022-11-03 14:02:46 +0100

    Add tests for release scripts
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- scripts/release/update-bsk-repo.js
+++ scripts/release/update-bsk-repo.js
@@ -1,40 +1,30 @@
 const {readFileSync, writeFileSync} = require('fs')
 const {join} = require('path')
-const {replaceInString} = require('./release-utils.js')
 const cwd = join(__dirname, '..')
 const filepath = (...path) => join(cwd, ...path)
+const {updatePackageResolved, updatePackageSwift} = require('./release-utils.js')
 
-const commit = process.env.GITHUB_SHA
-const version = process.env.VERSION
+const autofillCommit = process.env.GITHUB_SHA
+const autofillVersion = process.env.VERSION
 
 const packageSwiftPath = filepath('../../bsk/Package.swift')
-const autofillPackageSwiftRegex = new RegExp(
-    /(\.package\(name: "Autofill", url: "https:\/\/github.com\/duckduckgo\/duckduckgo-autofill\.git", \.exact\(")(.+)("\)\),)/
-)
 const packageResolvedPath = filepath('../../bsk/Package.resolved')
-const autofillPackageResolvedRegex = new RegExp(
-    /("package": "Autofill",\s+"repositoryURL": "https:\/\/github.com\/duckduckgo\/duckduckgo-autofill.git",\s+"state": {\s+"branch": null,\s+"revision": ")(\w+)(",\s+"version": ")([\d.]+)("\s+})/
-)
 
-function updateBSKRepo () {
+function updateBSKRepo (version, commit) {
     console.log('running updateBSKrepo')
+
+    if (!version) throw new Error('Version not provided')
+    if (!commit) throw new Error('Commit not provided')
+
     const packageSwift = readFileSync(packageSwiftPath, 'utf8')
-    const updatedPackageSwift = replaceInString(
-        packageSwift,
-        autofillPackageSwiftRegex,
-        `$1${version}$3`
-    )
+    const updatedPackageSwift = updatePackageSwift(packageSwift, version)
     writeFileSync(packageSwiftPath, updatedPackageSwift)
     console.log('Autofill reference updated in BSK\'s Package.swift')
 
     const packageResolved = readFileSync(packageResolvedPath, 'utf8')
-    const updatedPackageResolve = replaceInString(
-        packageResolved,
-        autofillPackageResolvedRegex,
-        `$1${commit}$3${version}$5`
-    )
+    const updatedPackageResolve = updatePackageResolved(packageResolved, version, commit)
     writeFileSync(packageResolvedPath, updatedPackageResolve)
     console.log('Autofill reference updated in BSK\'s Package.resolved')
 }
 
-updateBSKRepo()
+updateBSKRepo(autofillVersion, autofillCommit)
