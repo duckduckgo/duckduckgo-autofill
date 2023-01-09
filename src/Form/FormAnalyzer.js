68e0d5f73a5bb2076655948d3ae58c767c293d75
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-01-09 13:19:41 +0100

    Minor accuracy improvement
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/Form/FormAnalyzer.js
+++ src/Form/FormAnalyzer.js
@@ -5,7 +5,7 @@ import { getText, isLikelyASubmitButton } from '../autofill-utils.js'
 
 const negativeRegex = new RegExp(/sign(ing)?.?in(?!g)|log.?in|unsubscri|(forgot(ten)?|reset) (your )?password|password (forgotten|lost)/i)
 const positiveRegex = new RegExp(
-    /sign(ing)?.?up|join|\bregist(er|ration)|newsletter|\bsubscri(be|ption)|contact|create|start|enroll|settings|preferences|profile|update|checkout|guest|purchase|buy|order|schedule|estimate|request|new.?customer|(confirm|retype|repeat|reset) password|password confirm?/i
+    /sign(ing)?.?up|join|\bregist(er|ration)|newsletter|\bsubscri(be|ption)|contact|create|start|enroll|settings|preferences|profile|update|checkout|guest|purchase|buy|order|schedule|estimate|request|new.?customer|(confirm|retype|repeat) password|password confirm?/i
 )
 const conservativePositiveRegex = new RegExp(/sign.?up|join|register|enroll|newsletter|subscri(be|ption)|settings|preferences|profile|update/i)
 const strictPositiveRegex = new RegExp(/sign.?up|join|register|enroll|settings|preferences|profile|update/i)
