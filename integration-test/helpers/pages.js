314f3bc277efcb313e53d087f4d9634e6a6ba065
Shane Osbourne <sosbourne@duckduckgo.com>
2023-03-15 19:46:44 +0000

    wait for inputs to be decorated

--- integration-test/helpers/pages.js
+++ integration-test/helpers/pages.js
@@ -39,7 +39,7 @@ export function signupPage (page, server) {
             await page.goto(server.urlForPath(constants.pages['signup']))
         },
         async selectGeneratedPassword () {
-            const input = page.locator('#password')
+            const input = page.locator('#password[data-ddg-inputtype="credentials.password"]')
             await input.click()
 
             const passwordBtn = page.locator('button:has-text("Generated password")')
@@ -239,11 +239,13 @@ export function loginPage (page, server, opts = {}) {
          * @return {Promise<void>}
          */
         async selectFirstCredential (username) {
+            const decoratedEmailField = `#email[data-ddg-inputtype="credentials.username"]`;
             if (clickLabel) {
+                await page.waitForSelector(decoratedEmailField)
                 const label = page.locator('label[for="email"]')
                 await label.click()
             } else {
-                const email = page.locator('#email')
+                const email = page.locator(decoratedEmailField)
                 await email.click()
             }
 
@@ -298,7 +300,7 @@ export function loginPage (page, server, opts = {}) {
          */
         async assertFirstCredential (username, password) {
             await this.assertUsernameFilled(username)
-            await this.assertPasswordFilled((password))
+            await this.assertPasswordFilled(password)
         },
         async assertPasswordEmpty () {
             const passwordField = page.locator('#password')
