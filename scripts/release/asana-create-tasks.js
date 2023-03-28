c1563454bb9b0e50f2b628bf082617aa9852b925
Alistair Brown <github@alistairjcbrown.com>
2023-03-28 12:44:20 +0100

    Update Asana integration to include expected header

--- scripts/release/asana-create-tasks.js
+++ scripts/release/asana-create-tasks.js
@@ -58,7 +58,7 @@ let asana
 const setupAsana = () => {
     asana = Asana.Client.create({
         'defaultHeaders': {
-            'Asana-Enable': 'new_project_templates,new_user_task_lists'
+            'Asana-Enable': 'new_project_templates,new_user_task_lists,new_goal_memberships'
         }
     }).useAccessToken(ASANA_ACCESS_TOKEN)
 }
