93e81b94b2baed6171711ebe5e42ce2ab3607562
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-04-06 10:10:16 +0200

    Update release scripts with latest Apple tooling (#299)

--- scripts/release/update-apple-device-repo.js
+++ scripts/release/update-apple-device-repo.js
@@ -1,11 +1,15 @@
 const {readFileSync, writeFileSync} = require('fs')
 const {join} = require('path')
 const {updateProjectPbxproj} = require('./release-utils.js')
+const {updatePackageResolved} = require('./release-utils.js')
 const cwd = join(__dirname, '..')
 const filepath = (...path) => join(cwd, ...path)
 const platform = process.argv[2]
 
-const bskCommit = process.env.BSK_SHA
+const autofillCommit = process.env.GITHUB_SHA || ''
+const autofillVersion = process.env.VERSION || ''
+
+const bskCommit = process.env.BSK_SHA || ''
 
 function updateAppleDeviceRepo (platform = 'ios', commit) {
     console.log(`running updateAppleDeviceRepo for ${platform}`)
@@ -17,6 +21,25 @@ function updateAppleDeviceRepo (platform = 'ios', commit) {
     const projectFile = readFileSync(projectFilePath, 'utf8')
     const updatedProjectFile = updateProjectPbxproj(projectFile, commit)
     writeFileSync(projectFilePath, updatedProjectFile)
+
+    if (platform === 'macos') {
+        const packageResolvedPath = filepath('../../macos/DuckDuckGo.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved')
+
+        const substitutions = {
+            autofill: {
+                version: autofillVersion,
+                commit: autofillCommit
+            },
+            bsk: {
+                commit: bskCommit
+            }
+        }
+
+        const packageResolved = readFileSync(packageResolvedPath, 'utf8')
+        const updatedPackageResolve = updatePackageResolved(packageResolved, substitutions)
+        writeFileSync(packageResolvedPath, updatedPackageResolve)
+    }
+
     console.log(`BSK reference updated in ${platform} repo`)
 }
 
