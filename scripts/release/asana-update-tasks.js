49334cc3eea05f5a8201d6630282a7c694276c7f
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-05-05 13:57:54 +0200

    Ema/fix release again (#311)

--- scripts/release/asana-update-tasks.js
+++ scripts/release/asana-update-tasks.js
@@ -11,7 +11,6 @@ const prUrls = {
     bsk: process.env.BSK_PR_URL || 'error',
     ios: process.env.IOS_PR_URL || 'error',
     macos: process.env.MACOS_PR_URL || 'error',
-    extensions: process.env.EXTENSIONS_PR_URL || 'error',
     windows: process.env.WINDOWS_PR_URL || 'error'
 }
 const asanaOutputRaw = process.env.ASANA_OUTPUT || '{}'
