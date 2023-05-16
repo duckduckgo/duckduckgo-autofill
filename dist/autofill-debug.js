905afaf781a40e256a1383e5487700b0ea266923
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-05-16 12:28:18 +0200

    Update compiled file
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- dist/autofill-debug.js
+++ dist/autofill-debug.js
@@ -17433,7 +17433,7 @@ function createGlobalConfig(overrides) {
 
   let webkitMessageHandlerNames = []; // INJECT webkitMessageHandlerNames HERE
 
-  let isDDGTestMode = false; // INJECT isDDGTestMode HERE
+  let isDDGTestMode = false; isDDGTestMode = true;
 
   let contentScope = null;
   let userUnprotectedDomains = null;
