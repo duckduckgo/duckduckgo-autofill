264b624a380d931d39bb633c591c5935bef38bf4
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-07-25 13:53:11 +0200

    Update updater file
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- packages/password/scripts/rules.js
+++ packages/password/scripts/rules.js
@@ -1,7 +1,11 @@
 const {join} = require('path')
 const {writeFileSync} = require('fs')
-const outputPath = join(__dirname, '..', 'rules.json')
-const REMOTE_URL = 'https://raw.githubusercontent.com/apple/password-manager-resources/main/quirks/password-rules.json'
+const { createHash } = require('node:crypto')
+const outputPathRules = join(__dirname, '..', 'rules.json')
+const outputPathSharedCreds = join(__dirname, '..', 'shared-credentials.json')
+const BASE_URL = 'https://raw.githubusercontent.com/apple/password-manager-resources/main/quirks/'
+const SHARED_CREDENTIALS_URL = BASE_URL + 'shared-credentials.json'
+const PASSWORD_RULES_URL = BASE_URL + 'password-rules.json'
 
 /**
  * This file contains utilities for keeping our password rules in sync.
@@ -38,20 +42,22 @@ function summary (prev, next) {
 }
 
 /**
- * @param {typeof import("../rules.json")} rules
+ * @param {import("fs").PathOrFileDescriptor} outputPath
+ * @param {typeof import("../rules.json") | typeof import("../shared-credentials.json")} contents
  */
-function update (rules) {
-    writeFileSync(outputPath, JSON.stringify(rules, null, 2))
+function update (outputPath, contents) {
+    writeFileSync(outputPath, JSON.stringify(contents, null, 2))
 }
 
 /**
- * @returns {Promise<typeof import("../rules.json")>}
+ * @param {string} url
+ * @returns {Promise<any>}
  */
-function download () {
+function download (url) {
     const https = require('https')
     return new Promise((resolve, reject) => {
         const chunks = []
-        https.get(REMOTE_URL, (res) => {
+        https.get(url, (res) => {
             res.on('data', (d) => {
                 chunks.push(d.toString())
             })
@@ -63,16 +69,59 @@ function download () {
     })
 }
 
-if (process.argv.includes('--write-rules-json')) {
-    download()
+/**
+ * @returns {Promise<typeof import("../rules.json")>}
+ */
+function downloadRules () {
+    return download(PASSWORD_RULES_URL)
+}
+
+/**
+ * @returns {Promise<typeof import("../shared-credentials.json")>}
+ */
+function downloadSharedCreds () {
+    return download(SHARED_CREDENTIALS_URL)
+}
+
+/**
+ * Creates a hash of a string using node:crypto
+ * @param {string} content
+ * @param {string} algo
+ * @returns {string}
+ * @source https://stackoverflow.com/a/74933512/1948947
+ */
+function hashText (content, algo = 'sha256') {
+    const hashFunc = createHash(algo)
+    hashFunc.update(content)
+    return hashFunc.digest('hex')
+}
+
+if (process.argv.includes('--write-json-files')) {
+    downloadRules()
         .then((remoteRules) => {
             const current = require('../rules.json')
             const lines = summary(current, remoteRules)
             if (lines.length) {
-                update(remoteRules)
+                update(outputPathRules, remoteRules)
                 console.log('rules updated')
             } else {
-                console.log('nothing to update')
+                console.log('rules already up to date')
+            }
+        }).catch(e => {
+            console.error(e)
+            process.exit(1)
+        })
+
+    downloadSharedCreds()
+        .then((remoteSharedCreds) => {
+            const localSharedCreds = require('../shared-credentials.json')
+            const localSha256 = hashText(JSON.stringify(localSharedCreds))
+            const remoteSha256 = hashText(JSON.stringify(remoteSharedCreds))
+            if (localSha256 !== remoteSha256) {
+                update(outputPathSharedCreds, remoteSharedCreds)
+                console.log('shared credentials updated')
+            } else {
+                console.log('shared credentials already up to date')
             }
         }).catch(e => {
             console.error(e)
