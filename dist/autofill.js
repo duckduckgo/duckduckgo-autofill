857a2fe9136f258ce6b32824d29e53370ac6c368
Maxim Tsoy <maks.tsoy@gmail.com>
2023-08-04 18:06:31 +0200

    build assets

--- dist/autofill.js
+++ dist/autofill.js
@@ -11629,10 +11629,12 @@ class DefaultScanner {
 
 
   init() {
-    console.log('Autofill is enabled for this site');
-    this.device.deviceApi.notify(new _deviceApiCalls.AddDebugFlagCall({
-      flag: 'autofill'
-    }));
+    if (this.device.globalConfig.isExtension) {
+      this.device.deviceApi.notify(new _deviceApiCalls.AddDebugFlagCall({
+        flag: 'autofill'
+      }));
+    }
+
     const delay = this.options.initialDelay; // if the delay is zero, (chrome/firefox etc) then use `requestIdleCallback`
 
     if (delay === 0) {
