e5bd961a07fd44bf7a791ad59510cad4cda5fcd9
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-12-21 17:17:05 +0100

    Add integration tests for password update (#456)
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

---
+++ integration-test/tests/password-update.macos.spec.js
@@ -0,0 +1,81 @@
+import {createAutofillScript, forwardConsoleMessages} from '../helpers/harness.js'
+import {createWebkitMocks, macosContentScopeReplacements} from '../helpers/mocks.webkit.js'
+import {test as base} from '@playwright/test'
+import {createAvailableInputTypes} from '../helpers/utils.js'
+import {constants} from '../helpers/mocks.js'
+import {passwordUpdatePage} from '../helpers/pages/passwordUpdatePage.js'
+
+/**
+ *  Tests for various auto-fill scenarios on macos
+ */
+const test = base.extend({})
+
+const {personalAddress} = constants.fields.email
+const password = '123456'
+
+test.describe('Update password page', () => {
+    async function applyScript (page) {
+        await createAutofillScript()
+            .replaceAll(macosContentScopeReplacements())
+            .platform('macos')
+            .applyTo(page)
+    }
+
+    test('works as expected when there are saved credentials', async ({page}) => {
+        // enable in-terminal exceptions
+        await forwardConsoleMessages(page)
+        await createWebkitMocks()
+            .withAvailableInputTypes(createAvailableInputTypes())
+            .withCredentials({
+                id: '01',
+                username: personalAddress,
+                password,
+                credentialsProvider: 'duckduckgo'
+            })
+            .applyTo(page)
+
+        // Load the autofill.js script with replacements
+        await await applyScript(page)
+
+        const passwordUpdate = passwordUpdatePage(page)
+        await passwordUpdate.navigate()
+
+        // Check icons
+        await passwordUpdate.currentPasswordHasFillKey()
+        await passwordUpdate.newPasswordHasGenKey()
+        await passwordUpdate.confirmPasswordHasGenKey()
+
+        await passwordUpdate.fillCurrent()
+        await passwordUpdate.checkGeneratedFill()
+        // This happens afterwards because we want to make sure we didn't overwrite the current-password field
+        await passwordUpdate.checkCurrentFieldHasValue(password)
+    })
+
+    test('works as expected when there are no credentials', async ({page}) => {
+        // enable in-terminal exceptions
+        await forwardConsoleMessages(page)
+        await createWebkitMocks()
+            .withAvailableInputTypes(createAvailableInputTypes({
+                credentials: {
+                    username: false,
+                    password: false
+                }
+            }))
+            .applyTo(page)
+
+        // Load the autofill.js script with replacements
+        await await applyScript(page)
+
+        const passwordUpdate = passwordUpdatePage(page)
+        await passwordUpdate.navigate()
+
+        // Check icons
+        await passwordUpdate.currentFieldHasNoIcon()
+        await passwordUpdate.newPasswordHasGenKey()
+        await passwordUpdate.confirmPasswordHasGenKey()
+
+        await passwordUpdate.checkGeneratedFill()
+        // This happens afterwards because we want to make sure we didn't overwrite the current-password field
+        await passwordUpdate.checkCurrentFieldHasValue('')
+    })
+})
