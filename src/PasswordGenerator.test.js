2c0aa18e22ec05648353d6e09e8f84e42d810ab9
Sean Barag <sbarag@duckduckgo.com>
2024-05-14 11:23:34 -0700

    passwords: require a digit in passwords generated from default rules
    We previously required only a character in `[-!?$&#%]` when generating
    passwords for sites with neither a `passwordrules` attribute on their
    password input nor an override in `rules.json`. In many cases we'd
    include a number by random chance, but the relative weight of digits
    in the default pool meant they would sometimes not appear at all.
    Since sites with password content rules commonly require a digit to be
    present, this led to generated and saved passwords that immediately fail
    password validation. Require a digit for passwords generated from the
    default ruleset.

--- src/PasswordGenerator.test.js
+++ src/PasswordGenerator.test.js
@@ -14,4 +14,14 @@ describe('PasswordGenerator', () => {
         expect(new Set(pws).size).toBe(1)
         expect(pwg.generated).toBe(true)
     })
+
+    // Generate 10000 passwords and ensure they all include a number.
+    it('always includes a number', () => {
+        const digitRE = /[0-9]/
+        for (let i = 0; i < 10_000; i++) {
+            expect(
+                new PasswordGenerator().generate()
+            ).toMatch(digitRE)
+        }
+    })
 })
