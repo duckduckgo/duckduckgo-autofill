7af3c466fed9cc783251d77095b0c66e24ce1ad6
Shane Osbourne <sosbourne@duckduckgo.com>
2024-07-21 10:31:38 +0100

    linting

--- src/Scanner.debug.js
+++ src/Scanner.debug.js
@@ -42,7 +42,7 @@ const mockInterface = {
     isTooltipActive: () => {
         return false
     },
-    get scanner() {
+    get scanner () {
         return state.scanner
     }
 }
@@ -51,12 +51,12 @@ let state = {
     /** @type {import('./Scanner.js').Scanner | undefined} */
     scanner: undefined,
     /** @type {HTMLSelectElement|null} */
-    list: document.querySelector('select[name="html-list"]'),
+    list: document.querySelector('select[name="html-list"]')
 }
 
-const url = new URL(window.location.href);
-const initial = url.searchParams.get('form');
-const log = url.searchParams.has('log');
+const url = new URL(window.location.href)
+const initial = url.searchParams.get('form')
+const log = url.searchParams.has('log')
 
 if (log) {
     sessionStorage.setItem('ddg-autofill-debug', 'true')
@@ -71,47 +71,46 @@ loadList()
         }
     })
 
-
-async function loadList() {
-    const url = new URL(`/test-forms/index.json`, window.location.href);
+async function loadList () {
+    const url = new URL(`/test-forms/index.json`, window.location.href)
     fetch(url)
         .then(response => response.json())
         .then(x => {
             x.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.html;
-                option.textContent = item.html;
-                state.list?.appendChild(option);
-            });
+                const option = document.createElement('option')
+                option.value = item.html
+                option.textContent = item.html
+                state.list?.appendChild(option)
+            })
         })
 }
-async function loadNewForm(filename) {
-    const url = new URL(`/test-forms/${filename}`, window.location.href);
+async function loadNewForm (filename) {
+    const url = new URL(`/test-forms/${filename}`, window.location.href)
     fetch(url)
         .then(response => response.text())
         .then(html => {
-            let mainElement = document.querySelector('main');
+            let mainElement = document.querySelector('main')
             if (mainElement) {
-                mainElement.innerHTML = html;
+                mainElement.innerHTML = html
                 if (state.list) {
                     state.list.value = filename
                 }
                 state.scanner = createScanner(/** @type {any} */(mockInterface), {
                     initialDelay: 1 // allow debugging directly on macOS - if this was 0 then it would try to use requestIdleCallback, which is absent in WebKit
                 })
-                state.scanner?.init();
+                state.scanner?.init()
             } else {
-                console.log("'main' element not found on the page.");
+                console.log("'main' element not found on the page.")
             }
         })
         .catch(error => {
-            console.error('Error:', error);
-        });
+            console.error('Error:', error)
+        })
 }
 
 document.querySelector('select[name="html-list"]')?.addEventListener('change', (e) => {
-    const elem = /** @type {HTMLSelectElement} */(e.target);
-    const next = new URL(window.location.href);
-    next.searchParams.set('form', elem.value);
-    window.location.href = next.href;
+    const elem = /** @type {HTMLSelectElement} */(e.target)
+    const next = new URL(window.location.href)
+    next.searchParams.set('form', elem.value)
+    window.location.href = next.href
 })
