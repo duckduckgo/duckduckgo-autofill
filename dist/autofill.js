d30ed6ac048ac91fc6c308ec24029751107ce41c
dbajpeyi <dbajpeyi@duckduckgo.com>
2024-07-24 13:50:49 +0900

    feat: increase threshold for scanmalta

--- dist/autofill.js
+++ dist/autofill.js
@@ -13278,7 +13278,8 @@ const constants = exports.constants = {
   ATTR_AUTOFILL: 'data-ddg-autofill',
   TEXT_LENGTH_CUTOFF: 100,
   MAX_INPUTS_PER_PAGE: 100,
-  MAX_FORMS_PER_PAGE: 30,
+  // scanmalta.com/shop breaks below this threshold
+  MAX_FORMS_PER_PAGE: 36,
   MAX_INPUTS_PER_FORM: 80,
   MAX_FORM_RESCANS: 50
 };
--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,8 @@ export const constants = {
     ATTR_AUTOFILL: 'data-ddg-autofill',
     TEXT_LENGTH_CUTOFF: 100,
     MAX_INPUTS_PER_PAGE: 100,
-    MAX_FORMS_PER_PAGE: 30,
+    // scanmalta.com/shop breaks below this threshold
+    MAX_FORMS_PER_PAGE: 36,
     MAX_INPUTS_PER_FORM: 80,
     MAX_FORM_RESCANS: 50
 }
