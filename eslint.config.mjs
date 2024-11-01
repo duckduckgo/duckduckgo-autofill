fe265b26d669ebc78bc8ac0b6f4825c22e95ec36
Maxim Tsoy <maks.tsoy@gmail.com>
2024-11-01 12:24:19 +0100

    Upgrade eslint and depend on the shared ddg config

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import globals from "globals";
+import ddgConfig from "@duckduckgo/eslint-config";
+import babelParser from "@babel/eslint-parser";
+
+export default [{
+    ignores: [
+        "src/**/*.d.ts",
+        "dist/*",
+        "swift-package/Resources/assets/*",
+        "integration-test/extension/autofill.js",
+        "integration-test/extension/autofill-debug.js",
+        "src/deviceApiCalls/__generated__/*",
+        "src/Form/matching-config/__generated__/*",
+        "playwright-report/*",
+    ],
+},
+...ddgConfig,
+{
+    languageOptions: {
+        globals: {
+            ...globals.webextensions,
+            ...globals.browser,
+            ...globals.jasmine,
+            ...globals.jest,
+            windowsInteropPostMessage: "readonly",
+            windowsInteropAddEventListener: "readonly",
+            windowsInteropRemoveEventListener: "readonly",
+        },
+
+        parser: babelParser,
+        ecmaVersion: 2020,
+        sourceType: "module",
+
+        parserOptions: {
+            requireConfigFile: false,
+
+            babelOptions: {
+                configFile: "./babel.config.js",
+            },
+        },
+    },
+
+    rules: {
+        "no-restricted-globals": [2, "name"],
+
+        "import/extensions": ["error", "always", {
+            ignorePackages: true,
+        }],
+    },
+}];
