3cdbbb2f96c03c152221ba99a0c6fbc8a730c57e
dbajpeyi <dbajpeyi@duckduckgo.com>
2025-04-16 16:52:15 +0200

    transform css loader

--- dist/autofill.js
+++ dist/autofill.js
@@ -12503,7 +12503,7 @@ Source: "${matchedFrom}"`;
     }
   };
 
-  // src/UI/styles/autofill-tooltip-styles.css
+  // css-plugin:./styles/autofill-tooltip-styles.css
   var autofill_tooltip_styles_default = `:root {
     color-scheme: light dark;
 }
