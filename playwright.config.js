35b0b0b12fdd3e9f53ac3e195bdc8dd260aabc11
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2025-06-12 16:12:47 +0200

    Set to 4 workers
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ const config = {
     /* Retry on CI only */
     retries: process.env.CI ? 5 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 5 : 6,
+    workers: process.env.CI ? 4 : 6,
     // workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI ? 'github' : [['html', { open: 'never' }]],
