d8889855760b757f7e7395ddd87f0eb11dc2ca44
GioSensation <1828326+GioSensation@users.noreply.github.com>
2025-08-12 03:30:49 +0000

    Update password-related json files

--- dist/autofill-debug.js
+++ dist/autofill-debug.js
@@ -4778,6 +4778,9 @@ Source: "${matchedFrom}"`;
     "yatra.com": {
       "password-rules": "minlength: 8; required: lower; required: upper; required: digit; required: [!#$%&'()+,.:?@[_`~]];"
     },
+    "yeti.com": {
+      "password-rules": "minlength: 8; required: lower; required: upper; required: digit; required: [#$%*];"
+    },
     "zara.com": {
       "password-rules": "minlength: 8; required: lower; required: upper; required: digit;"
     },
