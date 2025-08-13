a0a7deda3d320db409c27f130510936cac28fb36
dbajpeyi <dbajpeyi@duckduckgo.com>
2025-08-13 16:04:41 +0200

    find previous comment

--- src/autofill-utils.js
+++ src/autofill-utils.js
@@ -594,7 +594,7 @@ function runWithTreeWalker(root, callback) {
     let node = walker.currentNode;
     while (node) {
         const result = callback(node);
-        if (result === true) return true;
+        if (result === true) return true; // If the callback returns true, we stop iterating
 
         node = walker.nextNode();
     }
